d4ba9022a923b534052dbfbc246f1dad14fc0d87
Konstantin Minchenkov <kanminchenkov@avito.ru>
2021-07-15 13:15:03 +0300

    added hack to deploy to heroku

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,4 @@ WORKDIR /go/src/github.com/illfalcon/spotiyan
 COPY . .
 RUN go build -o /go/bin/spotiyan cmd/main.go
 
-EXPOSE 8080
-
 ENTRYPOINT ["/go/bin/spotiyan"]
--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -34,5 +36,31 @@ func main() {
 		log.Fatal(err)
 	}
 
+	go herokuHack()
+
 	_ = tgBot.Listen()
+}
+
+func herokuHack() {
+	log.Print("starting server...")
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		name := os.Getenv("NAME")
+		if name == "" {
+			name = "World"
+		}
+		_, _ = fmt.Fprintf(w, "Hello %s!\n", name)
+	})
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
